0e80aaee04dd101ba53d747878632d8d5332f2fa
Iren1Paramonova <zerber17@ya.ru>
2022-11-10 17:44:12 +0300

    10/11

--- les8.js
+++ les8.js
@@ -1,24 +1,44 @@
-const multiArr = [1, 2, [3, 4, 5, [6, 7, 8]], 9, 10, [11, 12, [13, 14, [15]]]];
+/* const multiArr = [1, 2, [3, 4, 5, [6, 7, 8]], 9, 10, 15, [11, 12, [13, 14, [15]]]]; */
 //решение 1 в одну строку
-const firstTry = (arr) =>
+/* const firstTry = (arr) =>
   arr
     .toString()
     .split(",")
     .map((el) => +el);
 
-console.log(firstTry(multiArr));
+console.log(firstTry(multiArr)); */
 
-//решение 2 рекурсией
-const secondTry = (arr) => arr.flat(Infinity);
-console.log(secondTry(multiArr));
-const thirdTry = (arr) => {
+//решение 2
+/* const secondTry = (arr) => arr.flat(Infinity);
+console.log(secondTry(multiArr)); */
+
+//решение 3 рекурсией
+/* const thirdTry = (arr) => {
   let haveArr = false;
   for (let i = 0; i < arr.length; i += 1) {
     if (Array.isArray(arr[i])) {
       haveArr = true;
     }
   }
-};
-if (haveArr) {
+ if (haveArr) {
   return thirdTry(arr.flat());
+} return arr;};
+console.log(thirdTry(multiArr)); */
+
+//практика 1 задача
+/* let aArr = [2, 3, 5, 7, 8, 9, -4, -2, -1, 0, -8];
+const bArr = (arr) =>
+  arr
+    .filter((e) => e > 0)
+    .filter((e) => e % 2 == 0)
+    .reduce((accum, current) => accum + current);
+console.log(bArr(aArr)); */
+
+//практика 2 задача
+let arr = [1, 4, 9, 7, 6, 5, 8, 4, 3, 4, 6, 1];
+const unicArr = function (arr) {
+  let result =[];
+  for (let )
 }
+console.log(unicArr);
+//https://learn.javascript.ru/task/array-unique
