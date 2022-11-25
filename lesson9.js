48068170c9150588945e3335203ab9982104d4bd
Iren1Paramonova <zerber17@ya.ru>
2022-11-25 21:14:32 +0300

    25/11

--- lesson9.js
+++ lesson9.js
@@ -1,11 +1,14 @@
 function timer() {
   let start = 10;
+  let end = 5;
   return function (end) {
-    for (i = start; i > end; i -= 1) {
-      console.log(i);
+    for (; start > end; start -= 1) {
+      console.log(start);
     }
   };
 }
 const counting = timer();
-counting(5);
+
+setInterval(counting, 1000);
+
 console.log("Таймер закончил");
