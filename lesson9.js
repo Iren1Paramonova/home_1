70402d82b476b124f5bd602263c34dfefc8363f5
Iren1Paramonova <zerber17@ya.ru>
2022-11-30 17:33:31 +0300

    30/11

--- lesson9.js
+++ lesson9.js
@@ -12,3 +12,20 @@ const counting = timer();
 setInterval(counting, 1000);
 
 console.log("Таймер закончил");
+
+const timer=(start,end)=>{
+  let currentTime = start;
+  return function timerCounter(){
+    if(currentTime !== end){
+      console.log(currentTime);
+      setTimeout(()=>{
+        currentTime=-1;
+        timerCounter();
+      }, 1000)
+    }
+  }
+}
+const firstrTimer = timer (20,12);
+firstrTimer();
+
+
